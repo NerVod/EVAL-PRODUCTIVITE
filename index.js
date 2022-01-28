d191b4bd4b12c74d07c9860191fba1b14f4b8e77
NerVod <benjamin.jeannerot@gmail.com>
2022-01-28 14:53:06 +0100

    7:Renforcer le code

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-import cors from "cors";
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import cors from 'cors'
 
-const filename = fileURLToPath(import.meta.url);
-const dirname = path.dirname(filename);
-const port = 8070;
-const host = "127.0.0.1";
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+const port = 8070
+const host = '127.0.0.1'
 
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use(express.static(path.join(dirname, "public")));
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use(express.static(path.join(dirname, 'public')))
 app.use(
-  "/favicon.ico",
-  express.static(path.join(dirname, "public", "images", "favicon.png"))
-);
+  '/favicon.ico',
+  express.static(path.join(dirname, 'public', 'images', 'favicon.png'))
+)
 
-app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: path.join(dirname) }, (err) => {
-    if (err) throw new Error(err);
-  });
-});
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: path.join(dirname) }, (err) => {
+    if (err) throw new Error(err)
+  })
+})
 
-app.post("/comment", (req, res) => {
-  const comment = req.body.message;
-  res.send(comment);
-});
+app.post('/comment', (req, res) => {
+  const comment = req.body.message
+  let regex = /<script>/
+  let commentaire = comment.replace(regex, 'caractères interdits')
+  res.send(commentaire)
+})
 
 app.listen(port, host, () => {
-  console.info(`Server started @ http://${host}:${port}.`);
-});
+  console.info(`Server started @ http://${host}:${port}.`)
+})
