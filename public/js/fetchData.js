c6e2e3302da990e4d9f5d428e47e1d5215a9dc42
NerVod <benjamin.jeannerot@gmail.com>
2022-01-28 12:13:07 +0100

    Linter le code

--- public/js/fetchData.js
+++ public/js/fetchData.js
@@ -1,122 +1,153 @@
-window.addEventListener('DOMContentLoaded',()=>{
-  /** @type {html element} variable to store the name of the HTML element where pokemon name is loaded*/
-  const pokeP=document.getElementById('pokeInfo')
-  /** @type {string} name of the div placeholder where pokemon informations are loaded*/
-  const pokeDiv=document.getElementById('pokemon-info')
-  /** @type {html element} name of the button to listen to, to create new ability on user's click*/
-  const pokeAbilityBtn=document.getElementById('ability')
-  
+window.addEventListener("DOMContentLoaded", () => {
+  /** @type {html element} variable to store the name of the HTML element
+   * where pokemon name is loaded*/
+  const pokeP = document.getElementById("pokeInfo");
+  /** @type {string} name of the div placeholder where pokemon informations
+   * are loaded*/
+  const pokeDiv = document.getElementById("pokemon-info");
+  /** @type {html element} name of the button to listen to, to create new
+   * ability on user's click*/
+  const pokeAbilityBtn = document.getElementById("ability");
+
   /**
-   *Async function to find pokemon in pokeapi and insert text with the name of a pokemon in HTML page if a pokemon is found
+   *Async function to find pokemon in pokeapi and insert text with the name 
+   of a pokemon in HTML page if a pokemon is found
    *
-   @param {number} pokedexNum get random number to get random pokemon in api 
+   @param {number} pokedexNum get random number to get random pokemon in api
    @param {string} foundPokemon name of the pokemon
-   @param {string} jsonPokemon  will be used to store pokemon informations to create an object pokemon
+   @param {string} jsonPokemon  will be used to store pokemon informations to 
+   create an object pokemon
    @param {object} pokeInfo  object of the random selected pokemon
    *
    */
-  const fetchPokemon=async ()=>{
+  const fetchPokemon = async () => {
     /** @type {number} */
-    const pokedexNum=Math.floor(Math.random() * 897)
+    const pokedexNum = Math.floor(Math.random() * 897);
     /** @type {string} */
-    let foundPokemon=''
+    let foundPokemon = "";
     /** @type {string}*/
-    let jsonPokemon=''
+    let jsonPokemon = "";
     /** @type {object} */
-    const pokeInfo={}
+    const pokeInfo = {};
 
-    try{
-      foundPokemon=await fetch(`https://pokeapi.co/api/v2/pokemon/${pokedexNum}`, {method: 'GET', headers: {'Content-Type': 'application/json'}})
+    try {
+      foundPokemon = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${pokedexNum}`,
+        { method: "GET", headers: { "Content-Type": "application/json" } }
+      );
+    } catch (error) {
+      console.error(error.message);
     }
-    catch(error){console.error(error.message)}
 
-    if(foundPokemon){
-      try{
-        jsonPokemon=await foundPokemon.json()
-        pokeInfo.name=`${String(jsonPokemon.species.name).slice(0, 1).toUpperCase()}${String(jsonPokemon.species.name).slice(1, jsonPokemon.species.name.length).toLowerCase()}`
+    if (foundPokemon) {
+      try {
+        jsonPokemon = await foundPokemon.json();
+        pokeInfo.name = `${String(jsonPokemon.species.name)
+          .slice(0, 1)
+          .toUpperCase()}${String(jsonPokemon.species.name)
+          .slice(1, jsonPokemon.species.name.length)
+          .toLowerCase()}`;
+      } catch (error) {
+        console.error(error.message);
       }
-      catch(error){console.error(error.message)}
-
+    } else {
+      jsonPokemon = "No Pokémon found...";
     }
-    else{jsonPokemon='No Pokémon found...'}
 
-    if(pokeP.innerText !== ''){pokeP.innerText=''}
-    pokeP.innerText=`Your Pokémon is ${pokeInfo.name}.`
-    pokeAbilityBtn.removeAttribute('disabled')
-  }
+    if (pokeP.innerText !== "") {
+      pokeP.innerText = "";
+    }
+    pokeP.innerText = `Your Pokémon is ${pokeInfo.name}.`;
+    pokeAbilityBtn.removeAttribute("disabled");
+  };
 
   /**
-   * Asyn function to find pokemon abilities and insert it as text in HTML if an ability is found
-   * 
-   * @param {number} pokedexNum get random number to get random pokemon ability in api 
-   * @param {string} foundAbilities name of the ability for the pokemon 
-   * @param {string} pokeAbility id of HTML element where ability is loaded  
+   * Asyn function to find pokemon abilities and insert it as text in HTML if
+   * an ability is found
+   *
+   * @param {number} pokedexNum get random number to get random pokemon ability
+   * in api
+   * @param {string} foundAbilities name of the ability for the pokemon
+   * @param {string} pokeAbility id of HTML element where ability is loaded
    * @param {string} jsonAbilities  will be used to store ability informations
    * @param {string} abilityToDisplay  string of the ability to display in HTML
-   * 
+   *
    *
    */
-  const fetchPokemonAbilities=async()=>{
+  const fetchPokemonAbilities = async () => {
     /** @type {number} */
-    const pokedexNum=Math.floor(Math.random() * 266)
+    const pokedexNum = Math.floor(Math.random() * 266);
     /** @type {string} */
-    let foundAbilities=''
+    let foundAbilities = "";
     /** @type {string} */
-    const pokeAbility=document.getElementById('pokeAbility')
+    const pokeAbility = document.getElementById("pokeAbility");
     /** @type {string} */
-    let jsonAbilities={}
+    let jsonAbilities = {};
     /** @type {string} */
-    let abilityToDisplay=''
-  
+    let abilityToDisplay = "";
+
     try {
-      foundAbilities=await fetch(`https://pokeapi.co/api/v2/ability/${pokedexNum}`, {method: 'GET', headers: {'Content-Type': 'application/json'}})
+      foundAbilities = await fetch(
+        `https://pokeapi.co/api/v2/ability/${pokedexNum}`,
+        { method: "GET", headers: { "Content-Type": "application/json" } }
+      );
+    } catch (error) {
+      console.error(error.message);
     }
-    catch(error){console.error(error.message)}
 
-    if(foundAbilities){
-      try{
-        jsonAbilities=await foundAbilities.json();
-        if('' !== jsonAbilities.name && undefined !== jsonAbilities.name){
-          abilityToDisplay=`${String(jsonAbilities.name).slice(0, 1).toUpperCase()}${String(jsonAbilities.name).slice(1, jsonAbilities.name.length).toLowerCase()}`
+    if (foundAbilities) {
+      try {
+        jsonAbilities = await foundAbilities.json();
+        if ("" !== jsonAbilities.name && undefined !== jsonAbilities.name) {
+          abilityToDisplay = `${String(jsonAbilities.name)
+            .slice(0, 1)
+            .toUpperCase()}${String(jsonAbilities.name)
+            .slice(1, jsonAbilities.name.length)
+            .toLowerCase()}`;
+        } else {
+          abilityToDisplay = "Tackle";
         }
-        else{abilityToDisplay='Tackle'}
+      } catch (error) {
+        console.error(error.message);
       }
-      catch(error){console.error(error.message)}
-
+    } else {
+      jsonAbilities = "No ability found...";
     }
-    else{jsonAbilities='No ability found...'}
 
-    if(pokeAbility.innerText !== ''){pokeAbility.innerText=''}
+    if (pokeAbility.innerText !== "") {
+      pokeAbility.innerText = "";
+    }
 
-    pokeAbility.innerText=`It now knows the move ${abilityToDisplay}!`
-  }
-  
+    pokeAbility.innerText = `It now knows the move ${abilityToDisplay}!`;
+  };
+
   /**
    *function to create a pokemon on user's click
    *
-   * @param {string} pokeBtn string of the button name to listen to for user's click
+   * @param {string} pokeBtn string of the button name to listen to for user's
+   * click
    */
-  const invoquePokemon=()=>{
+  const invoquePokemon = () => {
     /** @type {string} */
-    const pokeBtn=document.getElementById('pokemon')
-    pokeBtn.addEventListener('click', fetchPokemon)
-    pokeDiv.appendChild(pokeP)
-  }
+    const pokeBtn = document.getElementById("pokemon");
+    pokeBtn.addEventListener("click", fetchPokemon);
+    pokeDiv.appendChild(pokeP);
+  };
 
   /**
    *function to create an ability on user's click
    *
    */
-  const pokemonAbility=()=>{
-    pokeAbilityBtn.addEventListener('click', fetchPokemonAbilities)
-    pokeDiv.appendChild(pokeAbility)
-  }
+  const pokemonAbility = () => {
+    pokeAbilityBtn.addEventListener("click", fetchPokemonAbilities);
+    pokeDiv.appendChild(pokeAbility);
+  };
   /**
-   *function to start functions to create pokemon and abilities on user's click 
+   *function to start functions to create pokemon and abilities on user's click
    *
    */
-  (function startAll(){
-    invoquePokemon()
-    pokemonAbility()
-  })()
-})
+  (function startAll() {
+    invoquePokemon();
+    pokemonAbility();
+  })();
+});
--- public/js/functionsToTest.js
+++ public/js/functionsToTest.js
@@ -2,23 +2,25 @@ const returnAnObject = (...args) => {
   let response = {};
   if (args.length) {
     let index = 0;
-    args.forEach(arg => {
+    args.forEach((arg) => {
       response[index] = arg;
       index++;
-    })
+    });
   } else {
-    response = 'No argument was given to the function.';
-  };
+    response = "No argument was given to the function.";
+  }
   return response;
 };
 
 const multiplyAllByTwo = (arrayOfNumbers) => {
   let response;
-  if (arrayOfNumbers.constructor.prototype === new Array().constructor.prototype) {
-    response = arrayOfNumbers.map((val => val * 2));
-    console.log('arrayTimesTwo: ', arrayTimesTwo);
+  if (
+    arrayOfNumbers.constructor.prototype === new Array().constructor.prototype
+  ) {
+    response = arrayOfNumbers.map((val) => val * 2);
+    console.log("arrayTimesTwo: ", arrayTimesTwo);
   } else {
-    response = 'The argument is not an Array of numbers';
-  };
+    response = "The argument is not an Array of numbers";
+  }
   return response;
 };
